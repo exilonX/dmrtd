c6bf25bb3b45d20023c91ce5f611cd198edc9773
exilonx <ionel.merca@gmail.com>
2025-07-10 15:02:00 +0300

    fix paceMode flag

--- lib/src/proto/pin_key.dart
+++ lib/src/proto/pin_key.dart
@@ -33,13 +33,13 @@ class PinKey extends AccessKey {
   Uint8List Kpi(CipherAlgorithm cipherAlgorithm, KEY_LENGTH keyLength) {
     switch ((cipherAlgorithm, keyLength)) {
       case (CipherAlgorithm.AES, KEY_LENGTH.s128):
-        return DeriveKey.aes128(_pinBytes, paceMode: true);
+        return DeriveKey.aes128(_pinBytes);
       case (CipherAlgorithm.AES, KEY_LENGTH.s192):
-        return DeriveKey.aes192(_pinBytes, paceMode: true);
+        return DeriveKey.aes192(_pinBytes);
       case (CipherAlgorithm.AES, KEY_LENGTH.s256):
-        return DeriveKey.aes256(_pinBytes, paceMode: true);
+        return DeriveKey.aes256(_pinBytes);
       case (CipherAlgorithm.DESede, _):
-        return DeriveKey.desEDE(_pinBytes, paceMode: true);
+        return DeriveKey.desEDE(_pinBytes);
       default:
         throw ArgumentError("Unsupported cipher/keyLength combo");
     }
